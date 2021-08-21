4687ee50986962bd80bb6ccfb840536ae8fdbec1
lijunjie <2655180794@123.com>
2021-08-21 23:51:14 +0800

    测试项目环境搭建v1

---
+++ src/main/java/com/baizhi/git2103/Git2103Application.java
@@ -0,0 +1,13 @@
+package com.baizhi.git2103;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git2103Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Git2103Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/baizhi/git2103/Git2103ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baizhi.git2103;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Git2103ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
